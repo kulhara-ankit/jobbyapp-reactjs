aa549bdcbcb5d4b1f72003bcb099a3599f62b7e4
kulhara-ankit <a.kulhara7@gmail.com>
2024-01-15 15:04:47 +0000

    updated and corrected code

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -12,7 +12,7 @@ const NotFound = () => (
       />
       <h1 className="not-found-heading">Page Not Found</h1>
       <p className="not-found-description">
-        we’re sorry, the page you requested could not be found
+        we are sorry, the page you requested could not be found
       </p>
     </div>
   </>
